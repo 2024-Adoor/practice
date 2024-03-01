f0fc421fb2a9c7c822ffdab7975114882cf70ba4
plwast-ints <plwast.ints@gmail.com>
2024-03-01 23:45:26 +0900

    2월 시연 영상용으로 만든 부분까지

---
+++ Assets/Scripts/PopupBookManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupBookManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void SpawnPopupBook()
+    {
+        // 팝업북을 생성하는 코드를 작성하세요
+    }
+}
